8f3b44803d32ce85b1cbbe2cdafdd82379694733
awerkamp <awerkamp@gmail.com>
2025-07-16 17:48:19 -0600

    update tailwind config to include all files by default and exclude only specific directories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./sites/**/*.{html,md}",
-    "./themes/overrides/**/*.html",
-    "./node_modules/flowbite/**/*.js"
+    // Scan all templates, content, and scripts
+    "./**/*.{html,md,js,ts,jsx,tsx}",
+
+    // Exclude bulky or generated folders
+    "!./node_modules/**/*",
+    "!./public/**/*",
+    "!./resources/**/*",
+
+    // But include Flowbite’s JS so its dynamic classes aren’t purged
+    "./node_modules/flowbite/**/*.js",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [
     require('flowbite/plugin'),
-    require('@tailwindcss/typography')
+    require('@tailwindcss/typography'),
   ],
 }
- 
